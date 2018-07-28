3808791586c5d448a3f920b0de6c5ec23c799e79
Ilia Makhonin <noizemkutane77@gmail.com>
2018-07-28 17:50:52 +0300

    Bot for testing was created

---
+++ main.py
@@ -0,0 +1,61 @@
+import telebot
+import json
+from test_analys import user_result, answer_reducer
+
+bot = telebot.TeleBot('token')
+
+keyboard = telebot.types.InlineKeyboardMarkup()
+one = telebot.types.InlineKeyboardButton(text='a', callback_data='А')
+two = telebot.types.InlineKeyboardButton(text='b', callback_data='В')
+keyboard.add(one, two)
+
+questions_list = []
+user_answer = {}
+
+def quest_put():
+    with open('questList.json', 'r', encoding='utf-8') as file:
+        questions = json.load(file)
+        for quest in questions:
+            questions_list.append(quest)
+
+
+@bot.message_handler(commands=['start'])
+def hello_bot(message):
+    chat_id = message.from_user.id
+    text = 'С помощью методики К.Н. Томаса (1973), американского социального психолога, определяются типические способы ' \
+           'реагирования на конфликтные ситуации. Можно выявить, насколько воспитатель склонен к соперничеству и ' \
+           'сотрудничеству в коллективе, стремится к компромиссам, избегает конфликтов, или, наоборот, старается ' \
+           'обострить их, а также оценить адаптации каждого члена коллектива к совместной педагогической деятельности.!' \
+           '\nВведите /continue, что бы начать тест'
+    bot.send_message(chat_id, text)
+
+
+@bot.message_handler(commands=['continue'])
+def test_start(message):
+    user_id = message.from_user.id
+    text = questions_list[0]
+    user_answer[user_id] = []
+    bot.send_message(user_id, text, reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def test_next(call):
+    user_id = call.from_user.id
+    index = questions_list.index(call.message.text) + 1
+    answer = str(index) + call.data
+    user_answer[user_id].append(answer)
+
+    if index >= len(questions_list):
+        text = 'Тест завершён!'
+        bot.send_message(user_id, text)
+        result = user_result(user_answer[user_id], answer_reducer)
+        bot.send_message(user_id, result)
+        return
+
+    text = questions_list[index]
+    bot.send_message(user_id, text, reply_markup=keyboard)
+
+
+if __name__ == '__main__':
+    quest_put()
+    bot.polling(none_stop=True)
---
+++ test_analys.py
@@ -0,0 +1,58 @@
+def answer_reducer(user_answer):
+    result = {
+        'rivalry': 0,
+        'cooperation': 0,
+        'compromise': 0,
+        'avoidance': 0,
+        'adjustment': 0
+    }
+
+    key_test = {
+        'rivalry': ['3А', '6В', '8А', '9В', '10А', '13В', '14В', '16В', '17А', '22В', '25А', '28А'],
+        'cooperation': ['2В', '5А', '8В', '11А', '14А', '19А', '20А', '21В', '23В', '26В', '28В', '30В'],
+        'compromise': ['2А', '4А', '7В', '10В', '12В', '13А', '18В', '22А', '23А', '24В', '26А', '29А'],
+        'avoidance': ['1А', '5В', '6А', '7А', '9А', '12А', '15В', '17В', '19В', '20В', '27А', '29В'],
+        'adjustment': ['1В', '3В', '4В', '11В', '15А', '16А', '18А', '21А', '24А', '25В', '27В', '30А']
+    }
+
+    for answer in user_answer:
+        for scale in key_test:
+            if answer in key_test[scale]:
+                result[scale] += 1
+
+    return result
+
+
+def user_result(test_result, test_reducer):
+    result = test_reducer(test_result)
+    result_sorted = []
+    scale_sorted = []
+    result_sort_value = []
+    for values in sorted(result.values(), reverse=True):
+        for key in result:
+            if result[key] == values and key not in result_sort_value:
+                result_sorted.append(values)
+                result_sort_value.append(key)
+                if key == 'rivalry':
+                    scale_sorted.append('соперничество')
+                if key == 'adjustment':
+                    scale_sorted.append('приспособление')
+                if key == 'compromise':
+                    scale_sorted.append('компромисс')
+                if key == 'cooperation':
+                    scale_sorted.append('сотрудничество')
+                if key == 'avoidance':
+                    scale_sorted.append('избегание')
+
+    conclusion = 'По результатам тестирования можно говорить о следующем:\n' \
+                 'Максимальный бал был набран по шкале "{}" - {}\n' \
+                 'По шкале "{}" набрано - {} баллов\n' \
+                 'По шкале "{}" - {}, по шкале "{}" - {}\n' \
+                 'Минимальный бал был набран по шкале "{}" - {}'.format(
+        scale_sorted[0], result_sorted[0],
+        scale_sorted[1], result_sorted[1],
+        scale_sorted[2], result_sorted[2],
+        scale_sorted[3], result_sorted[3],
+        scale_sorted[4], result_sorted[4])
+
+    return conclusion
